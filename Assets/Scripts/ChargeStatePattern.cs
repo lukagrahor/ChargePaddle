f4600dc2688d016264ad7486b862144a6b104ee1
Gaber <41000453+lukagrahor@users.noreply.github.com>
2024-11-17 16:24:21 +0100

    nekej

--- Assets/Scripts/ChargeStatePattern.cs
+++ Assets/Scripts/ChargeStatePattern.cs
@@ -46,6 +46,7 @@ public class ChargeStatePattern : MonoBehaviour, IChargeContext
 public class NormalBounceState : IChargeStatePattern
 {
     public void StartCharging(IChargeContext context) {
+        ChargePaddle(1);
         context.SetState(new ChargeTransitionState());
     }
     public void StopCharging(IChargeContext context) { }
@@ -53,6 +54,57 @@ public class NormalBounceState : IChargeStatePattern
     public void EndPowerTime(IChargeContext context) { }
     public void RunOutOfTime(IChargeContext context) { }
     public void PerformAChargedBounce(IChargeContext context) { }
+
+    int chargeLevel = 0;
+    float[] chargeSizes = new float[3];
+    float[] chargeSizeChanges = { 1f, 0.9f, 0.6f };
+    float[] resizeDurations = { 0.5f, 0.3f, 0.2f };
+    float chargeTimer = 0f;
+
+    void ChargePaddle(float currentSize)
+    {
+        if (chargeLevel < 3)
+        {
+            float currentResizeDuration = resizeDurations[chargeLevel];
+        }
+        /*
+        if (chargeLevel < 3)
+        {
+            float currentResizeDuration = resizeDurations[chargeLevel];
+            chargeTimer = chargeTimer < currentResizeDuration ? chargeTimer + Time.deltaTime : currentResizeDuration;
+            currentSize = chargeLevel > 0 ? chargeSizes[chargeLevel - 1] : currentSize;
+            float nextSize = chargeSizes[chargeLevel];
+
+            if (chargeTimer >= currentResizeDuration)
+            {
+                waitBetweenCharges -= Time.deltaTime;
+                // med vsakim nivojem charge-a je kratek èasovni interval, ko se ob zadetku žogica pohitri
+                if (waitBetweenCharges <= 0f)
+                {
+                    waitBetweenCharges = 0.5f;
+                    chargeTimer = 0f;
+                    chargeLevel++;
+                    chargeMultiplier = 1f;
+                }
+                else
+                {
+                    // tle pospešimo žogco
+                    chargeMultiplier = 1f + ((chargeLevel + 1) * 0.25f);
+                }
+            }
+            else
+            {
+                SetSize(Mathf.Lerp(currentSize, nextSize, chargeTimer / currentResizeDuration));
+            }
+        }
+        else
+        {
+            chargeOvertime += Time.deltaTime;
+            chargeInterrupt = chargeOvertime >= 0.5f ? true : false;
+            chargeMultiplier = 2f;
+        }
+        */
+    }
 
 }
 
@@ -68,58 +120,6 @@ public class ChargeTransitionState : IChargeStatePattern
     public void EndPowerTime(IChargeContext context) { }
     public void RunOutOfTime(IChargeContext context) { }
     public void PerformAChargedBounce(IChargeContext context) { }
-
-    int chargeLevel = 0;
-    float[] chargeSizes = new float[3];
-    float[] chargeSizeChanges = { 1f, 0.9f, 0.6f };
-    float[] resizeDurations = { 0.5f, 0.3f, 0.2f };
-    float chargeTimer = 0f;
-
-
-    void ChargePaddle(float currentSize)
-    {
-        if (chargeLevel < 3)
-        {
-            float currentResizeDuration = resizeDurations[chargeLevel];
-        }
-            /*
-            if (chargeLevel < 3)
-            {
-                float currentResizeDuration = resizeDurations[chargeLevel];
-                chargeTimer = chargeTimer < currentResizeDuration ? chargeTimer + Time.deltaTime : currentResizeDuration;
-                currentSize = chargeLevel > 0 ? chargeSizes[chargeLevel - 1] : currentSize;
-                float nextSize = chargeSizes[chargeLevel];
-
-                if (chargeTimer >= currentResizeDuration)
-                {
-                    waitBetweenCharges -= Time.deltaTime;
-                    // med vsakim nivojem charge-a je kratek èasovni interval, ko se ob zadetku žogica pohitri
-                    if (waitBetweenCharges <= 0f)
-                    {
-                        waitBetweenCharges = 0.5f;
-                        chargeTimer = 0f;
-                        chargeLevel++;
-                        chargeMultiplier = 1f;
-                    }
-                    else
-                    {
-                        // tle pospešimo žogco
-                        chargeMultiplier = 1f + ((chargeLevel + 1) * 0.25f);
-                    }
-                }
-                else
-                {
-                    SetSize(Mathf.Lerp(currentSize, nextSize, chargeTimer / currentResizeDuration));
-                }
-            }
-            else
-            {
-                chargeOvertime += Time.deltaTime;
-                chargeInterrupt = chargeOvertime >= 0.5f ? true : false;
-                chargeMultiplier = 2f;
-            }
-            */
-     }
 }
 
 public class PowerfulChargeState : IChargeStatePattern
